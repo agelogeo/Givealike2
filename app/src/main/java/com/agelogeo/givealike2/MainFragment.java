23cc108feb125220c000776bc64c991198826946
Giorgos Angelopoulos <agelogeo@gmail.com>
2019-03-18 11:53:12 +0200

    DownloadTask added. ImageDownloader added.

--- app/src/main/java/com/agelogeo/givealike2/MainFragment.java
+++ app/src/main/java/com/agelogeo/givealike2/MainFragment.java
@@ -1,12 +1,36 @@
 package com.agelogeo.givealike2;
 
+import android.content.ClipDescription;
+import android.content.ClipboardManager;
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.support.constraint.ConstraintLayout;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 
 /**
@@ -22,6 +46,14 @@ public class MainFragment extends Fragment {
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
+
+    FloatingActionButton fab;
+    ConstraintLayout imageConstraint;
+    Spinner spinner;
+    Button getHashtagsBtn;
+    TextView hashtagsView,customUsername,customLikeView;
+    ImageView customPhotoWallpaper,customProfile;
+    String link = "https://www.instagram.com/p/BvAkJnoDwmF/";
 
     // TODO: Rename and change types of parameters
     private String mParam1;
@@ -58,13 +90,54 @@ public class MainFragment extends Fragment {
             mParam1 = getArguments().getString(ARG_PARAM1);
             mParam2 = getArguments().getString(ARG_PARAM2);
         }
+
+
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_main, container, false);
+        View view = inflater.inflate(R.layout.fragment_main, container, false);
+
+        fab = view.findViewById(R.id.pasteButton);
+        imageConstraint = view.findViewById(R.id.imageConstraintLayout);
+        spinner = view.findViewById(R.id.spinner);
+        getHashtagsBtn = view.findViewById(R.id.getHashtagsButton);
+        hashtagsView = view.findViewById(R.id.hashtagsTextView);
+        customPhotoWallpaper = view.findViewById(R.id.custom_photoWallpaper);
+        customProfile = view.findViewById(R.id.custom_profilePicView);
+        customUsername = view.findViewById(R.id.custom_usernameTextView);
+        customLikeView = view.findViewById(R.id.customLikeView);
+
+
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                link = readFromClipboard();
+                DownloadTask task = new DownloadTask();
+                try {
+                    task.execute(link);
+                }catch (Exception e){
+                    e.printStackTrace();
+                }
+            }
+        });
+
+        getHashtagsBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                hashtagsView.setVisibility(View.VISIBLE);
+            }
+        });
+
+        return view;
+    }
+
+    public void updateViewAfterPaste(){
+        imageConstraint.setVisibility(View.VISIBLE);
+        spinner.setVisibility(View.VISIBLE);
+        getHashtagsBtn.setVisibility(View.VISIBLE);
+        hashtagsView.setVisibility(View.GONE);
     }
 
     // TODO: Rename method, update argument and hook method into UI event
@@ -95,4 +168,132 @@ public class MainFragment extends Fragment {
         // TODO: Update argument type and name
         void onFragmentInteraction(Uri uri);
     }
+
+    public String readFromClipboard() {
+        ClipboardManager clipboard = (ClipboardManager) getContext().getSystemService(Context.CLIPBOARD_SERVICE);
+        if (clipboard.hasPrimaryClip()) {
+            android.content.ClipDescription description = clipboard.getPrimaryClipDescription();
+            android.content.ClipData data = clipboard.getPrimaryClip();
+            if (data != null && description != null && description.hasMimeType(ClipDescription.MIMETYPE_TEXT_PLAIN))
+                return String.valueOf(data.getItemAt(0).getText());
+        }
+        Toast.makeText(getContext(),"Please copy a valid link.", Toast.LENGTH_SHORT).show();
+        return "";
+    }
+
+    public class DownloadTask extends AsyncTask<String,Void ,String> {
+
+        @Override
+        protected String doInBackground(String... urls) {
+            String result = "",line = null;
+            URL url;
+            HttpURLConnection urlConnection = null;
+
+            try{
+                url = new URL(urls[0]);
+                urlConnection = (HttpURLConnection) url.openConnection();
+                BufferedReader reader = new BufferedReader(new InputStreamReader(urlConnection.getInputStream()));
+
+                while ((line = reader.readLine()) != null)
+                    result += line;
+
+                return result;
+            }catch (Exception e){
+                e.printStackTrace();
+                return null;
+            }
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            analyzePost(s);
+        }
+    }
+
+    public void analyzePost(String s){
+        try{
+            //Log.i("analyze",s);
+            String link ;
+            Pattern pattern = Pattern.compile("window._sharedData = (.*?)[}];");
+            Matcher matcher = pattern.matcher(s);
+
+            matcher.find();
+            String jObject = matcher.group(1)+"}";
+            JSONObject jsonObject = new JSONObject(jObject);
+            JSONObject entry_data = jsonObject.getJSONObject("entry_data");
+            JSONArray PostPage = entry_data.getJSONArray("PostPage");
+            JSONObject first_graphql_shortcode_media = PostPage.getJSONObject(0).getJSONObject("graphql").getJSONObject("shortcode_media");
+            JSONObject owner = first_graphql_shortcode_media.getJSONObject("owner");
+
+            Log.i("USERNAME",owner.getString("username"));
+            Log.i("PROFILE_PIC_URL",owner.getString("profile_pic_url"));
+            customUsername.setText(owner.getString("username"));
+            customLikeView.setText(first_graphql_shortcode_media.getJSONObject("edge_media_preview_like").getString("count"));
+
+            if(first_graphql_shortcode_media.has("edge_sidecar_to_children")){
+                JSONArray children_edges = first_graphql_shortcode_media.getJSONObject("edge_sidecar_to_children").getJSONArray("edges");
+                Log.i("WITH_CHILDREN_COUNT",Integer.toString(children_edges.length()));
+
+                for(int i=0; i<2; i++){
+                    JSONObject node = children_edges.getJSONObject(i).getJSONObject("node");
+
+                    if(node.has("video_url")){
+                        //link = node.getString("video_url");
+                        link = node.getJSONArray("display_resources").getJSONObject(2).getString("src");
+                        Log.i("CHILDREN_W_VIDEO_"+(i+1),node.getString("video_url"));
+                    }else{
+                        link = node.getJSONArray("display_resources").getJSONObject(2).getString("src");
+                        Log.i("CHILDREN_W_PHOTO_"+(i+1),node.getJSONArray("display_resources").getJSONObject(2).getString("src"));
+                    }
+                    ImageDownloader imageTask = new ImageDownloader();
+                    imageTask.execute(link);
+                }
+            }else{
+                if(first_graphql_shortcode_media.has("video_url")){
+                    Log.i("NO_CHILDREN_W_VIDEO",first_graphql_shortcode_media.getString("video_url"));
+                    link = first_graphql_shortcode_media.getJSONArray("display_resources").getJSONObject(2).getString("src");
+                }else{
+                    Log.i("NO_CHILDREN_W_PHOTO",first_graphql_shortcode_media.getJSONArray("display_resources").getJSONObject(2).getString("src"));
+                    link = first_graphql_shortcode_media.getJSONArray("display_resources").getJSONObject(2).getString("src");
+                }
+                ImageDownloader imageTask = new ImageDownloader();
+                imageTask.execute(link);
+            }
+
+        }catch (Exception e){
+            Toast.makeText(getContext(),"Error with your link.",Toast.LENGTH_SHORT).show();
+            e.printStackTrace();
+        }
+    }
+
+
+    public class ImageDownloader extends AsyncTask<String,Void, Bitmap> {
+
+        @Override
+        protected Bitmap doInBackground(String... urls) {
+            try{
+                URL url = new URL(urls[0]);
+                HttpURLConnection httpURLConnection = (HttpURLConnection) url.openConnection();
+                httpURLConnection.connect();
+
+                InputStream inputStream = httpURLConnection.getInputStream();
+                Bitmap myBitmap = BitmapFactory.decodeStream(inputStream);
+
+                return myBitmap;
+
+            }catch (Exception e){
+                e.printStackTrace();
+                return null;
+            }
+        }
+
+        @Override
+        protected void onPostExecute(Bitmap bitmap) {
+            customPhotoWallpaper.setImageBitmap(bitmap);
+            updateViewAfterPaste();
+            //imageGrid.setAdapter(new ImageAdapter(getActivity().getApplicationContext(), bitmapList));
+
+        }
+    }
+
 }
