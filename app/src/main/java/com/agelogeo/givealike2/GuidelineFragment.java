e1a3f33997eb7ff01c945397031add2849280920
agelogeo <agelogeo@gmail.com>
2019-03-24 12:44:13 +0200

    GuidelineFragment semi finished.

--- app/src/main/java/com/agelogeo/givealike2/GuidelineFragment.java
+++ app/src/main/java/com/agelogeo/givealike2/GuidelineFragment.java
@@ -2,28 +2,41 @@ package com.agelogeo.givealike2;
 
 
 import android.os.Bundle;
+import android.support.constraint.ConstraintLayout;
 import android.support.v4.app.Fragment;
+import android.support.v4.view.ViewPager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class GuidelineFragment extends Fragment {
-
+    private ViewPager mSlideViewPager;
+    private ConstraintLayout mDotLayout;
+    private SliderAdapter sliderAdapter;
 
     public GuidelineFragment() {
         // Required empty public constructor
+
     }
 
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_guideline, container, false);
+        View v = inflater.inflate(R.layout.fragment_guideline, container, false);
+        mSlideViewPager = v.findViewById(R.id.slideViewPager);
+        mDotLayout = v.findViewById(R.id.dotsLayout);
+
+        sliderAdapter = new SliderAdapter(getContext());
+
+        mSlideViewPager.setAdapter(sliderAdapter);
+
+        return v;
     }
 
 }
---
+++ app/src/main/java/com/agelogeo/givealike2/SliderAdapter.java
@@ -0,0 +1,57 @@
+package com.agelogeo.givealike2;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.constraint.ConstraintLayout;
+import android.support.v4.view.PagerAdapter;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+public class SliderAdapter extends PagerAdapter {
+
+    Context context;
+    LayoutInflater layoutInflater;
+
+    public SliderAdapter(Context context) {
+        this.context = context;
+    }
+
+    // Arrays
+    public int[] slide_images = {
+            R.drawable.givealike_1,
+            R.drawable.givealike_2,
+            R.drawable.givealike_3,
+            R.drawable.givealike_4
+    };
+
+    @Override
+    public int getCount() {
+        return slide_images.length;
+    }
+
+    @Override
+    public boolean isViewFromObject(@NonNull View view, @NonNull Object o) {
+        return view == o;
+    }
+
+    @NonNull
+    @Override
+    public Object instantiateItem(@NonNull ViewGroup container, int position) {
+        layoutInflater = (LayoutInflater) context.getSystemService(context.LAYOUT_INFLATER_SERVICE);
+        View view = layoutInflater.inflate(R.layout.slide_layout,container,false);
+
+        ImageView slideImageView = view.findViewById(R.id.slide_image);
+        slideImageView.setImageResource(slide_images[position]);
+
+        container.addView(view);
+
+        return view;
+    }
+
+    @Override
+    public void destroyItem(@NonNull ViewGroup container, int position, @NonNull Object object) {
+        container.removeView((ConstraintLayout) object);
+    }
+}
